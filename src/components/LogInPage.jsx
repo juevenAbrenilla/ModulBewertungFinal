9259984ea8d3016415565ea21075cb60db703f09
juevenAbrenilla <jueven1178@gmail.com>
2024-07-16 14:25:01 +0200

    LogIn Page updated

--- src/components/LogInPage.jsx
+++ src/components/LogInPage.jsx
@@ -4,19 +4,40 @@ class LogInPage extends Component {
   state = {};
 
   render() {
-    const backgroundImage = require("./images/loginbackground.jpg");
+    const backgroundImage = require("../images/loginbackground.jpg");
 
     return (
-      <div
-        style={{
-          height: "100vh",
-          backgroundImage: `url(${backgroundImage})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-      >
-        {/* Add your content here */}
-      </div>
+      <>
+        <div
+          style={{
+            height: "100vh",
+            backgroundImage: `url(${backgroundImage})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        ></div>
+        <div className="container">
+          <div className="signIn-container">
+            <div className="row text-center">
+              <h1>Sign In</h1>
+              <div>
+                <a href="/">
+                  <img src="./images/microsoftlogo.jpg" alt="..." />
+                </a>
+              </div>
+              <div>
+                <span>
+                  No account? <a href="/">Create one!</a>
+                </span>
+              </div>
+              <a href="/">Can't access your account?</a>
+              <button className="button-exit" href="/">
+                Exit
+              </button>
+            </div>
+          </div>
+        </div>
+      </>
     );
   }
 }
