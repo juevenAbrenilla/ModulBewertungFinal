0feff75857aca54d7c65f4a18dfa602437d26120
juevenAbrenilla <jueven1178@gmail.com>
2024-07-18 11:37:36 +0200

    MainMenuwithHeaderzusammen

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import React, { Fragment } from "react";
 import { Routes, Route } from "react-router-dom";
 import LogInPage from "./components/LogInPage";
-import Header from "./components/Header";
-import MainMenu from "./components/MainMenu";
+import MainPageWithHeader from "./components/MainPageWithHeader";
 import ModulBewertungErstellen from "./components/ModulBewertungErstellen";
 import ModulBewertung from "./components/ModulBewertung";
 import ModulBewertungListe from "./components/ModulBewertungListe";
@@ -11,17 +10,16 @@ import LogOutPage from "./components/LogOutPage";
 const App = () => {
   return (
     <>
-      <Header />
       <Routes>
         <Fragment>
           <Route index element={<LogInPage />} />
-          <Route path="/mainmenu" element={<MainMenu />} />
+          <Route path="/MainPageWithHeader" element={<MainPageWithHeader />} />
           <Route
             path="/ModulBewertungErstellen"
             element={<ModulBewertungErstellen />}
           />
           <Route path="/ModulBewertung" element={<ModulBewertung />} />
-          <Route path="/MainMenu" element={<MainMenu />} />
+
           <Route
             path="/ModulBewertungListe"
             element={<ModulBewertungListe />}
