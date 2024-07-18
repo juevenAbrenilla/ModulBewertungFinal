ecda0eedebc4152b31abc02c1664d366459eab44
juevenAbrenilla <jueven1178@gmail.com>
2024-07-18 13:05:41 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import LogInPage from "./components/LogInPage";
 import MainPageWithHeader from "./components/MainPageWithHeader";
 import ModulBewertungErstellen from "./components/ModulBewertungErstellen";
@@ -10,24 +10,22 @@ import LogOutPage from "./components/LogOutPage";
 const App = () => {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Fragment>
-            <Route index element={<LogInPage />} />
-            <Route path="MainPageWithHeader" element={<MainPageWithHeader />} />
-            <Route
-              path="/ModulBewertungErstellen"
-              element={<ModulBewertungErstellen />}
-            />
-            <Route path="/ModulBewertung" element={<ModulBewertung />} />
-            <Route
-              path="/ModulBewertungListe"
-              element={<ModulBewertungListe />}
-            />
-            <Route path="/LogOutPage" element={<LogOutPage />} />
-          </Fragment>
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Fragment>
+          <Route index element={<LogInPage />} />
+          <Route path="MainPageWithHeader" element={<MainPageWithHeader />} />
+          <Route
+            path="/ModulBewertungErstellen"
+            element={<ModulBewertungErstellen />}
+          />
+          <Route path="/ModulBewertung" element={<ModulBewertung />} />
+          <Route
+            path="/ModulBewertungListe"
+            element={<ModulBewertungListe />}
+          />
+          <Route path="/LogOutPage" element={<LogOutPage />} />
+        </Fragment>
+      </Routes>
     </>
   );
 };
